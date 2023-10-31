11768e031c30ca68555ae12a6091ad3f9a22adf9
edupoly <info.edupoly@gmail.com>
2023-10-31 08:02:50 +0530

    sdsd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'edupoly',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  x:any;
-  username:FormControl = new FormControl('',[Validators.required]);
-  
+  constructor(public fb:FormBuilder){}
+  technologies=['nodejs','angular','react','express','mongodb'];
+  temp:any[]=[];
+  studentForm=this.fb.group({
+    fullname:[],
+    gender:[],
+    techs:this.fb.array([])
+  })
+  ngOnInit(){
+    this.technologies.forEach((t)=>{
+      this.techs.push(this.fb.control(false))
+    })
+  }
+  get techs(){
+    return this.studentForm.get('techs') as FormArray
+  }
+
+  updateTechnolgies(e:any){
+    this.temp.push(e.target.value);
+    console.log(this.temp)
+    // this.studentForm.controls.techs.patchValue(...this.temp)
+  }
 }
 
