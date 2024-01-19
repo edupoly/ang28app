fccd1bc41c9ada6dcd17d2eaaff788b3c8965015
edupoly <info.edupoly@gmail.com>
2024-01-19 17:07:56 +0530

    bacc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { CountriesService } from './countries.service';
 import { PostService } from './post.service';
 import { Router } from '@angular/router';
-import { initializeApp } from "firebase/app";
 
 @Component({
   selector: 'edupoly',
@@ -10,28 +9,21 @@ import { initializeApp } from "firebase/app";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // Import the functions you need from the SDKs you need
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-firebaseConfig = {
-  apiKey: "AIzaSyDRMr3FH2ViAyOIZmv94QgoeaBrVM_62BQ",
-  authDomain: "ang28authapp.firebaseapp.com",
-  projectId: "ang28authapp",
-  storageBucket: "ang28authapp.appspot.com",
-  messagingSenderId: "178046339065",
-  appId: "1:178046339065:web:1c03476100729295521f28"
-};
 
 // Initialize Firebase
 constructor(public router:Router){
-  initializeApp(this.firebaseConfig);
 }
   loginStatus:boolean=false;
+  displayName:string|null='';
+  photoURL:string|null='';
   ngDoCheck(){
     if(window.localStorage['logged']){
       this.loginStatus=true;
+    }
+    if(window.localStorage.getItem('displayName')){
+      this.displayName=window.localStorage.getItem('displayName');
+      this.photoURL=window.localStorage.getItem('photoURL');
     }
   }
   ngOnInit(){
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
-import { GoogleAuthProvider,signInWithPopup } from 'firebase/auth';
+import { GoogleAuthProvider,signInWithPopup,getAuth } from 'firebase/auth';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { initializeApp } from "firebase/app";
 
 @Component({
   selector: 'app-login',
@@ -8,19 +9,35 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+    
+firebaseConfig = {
+  apiKey: "AIzaSyDRMr3FH2ViAyOIZmv94QgoeaBrVM_62BQ",
+  authDomain: "ang28authapp.firebaseapp.com",
+  projectId: "ang28authapp",
+  storageBucket: "ang28authapp.appspot.com",
+  messagingSenderId: "178046339065",
+  appId: "1:178046339065:web:1c03476100729295521f28"
+};
   userDetails={
     email:'',
     password:''
   }
   constructor(public router:Router){
-    var provider = new GoogleAuthProvider();
-    signInWithPopup()
+    
   }
   login(){
     window.localStorage.setItem('logged','true');
     this.router.navigate(['/countries']);
   }
   loginWithGoogle(){
-
+    var app = initializeApp(this.firebaseConfig);
+    var provider = new GoogleAuthProvider();
+    var auth = getAuth(app);
+    signInWithPopup(auth,provider).then((result:any)=>{
+      console.log(result)
+      window.localStorage.setItem("token",result['user']['accessToken'])
+      window.localStorage.setItem("displayName",result['user']['displayName'])
+      window.localStorage.setItem("photoURL",result['user']['photoURL'])
+    })
   }
 }
